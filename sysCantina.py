e9a982d62ae36df04cfe5f6f8a0e322bc4526659
maximo frias <129902079+maximofrias522@users.noreply.github.com>
2023-06-29 19:41:40 -0300

    19:41

---
+++ sysCantina.py
@@ -0,0 +1,12 @@
+import sqlite3
+import uuid 
+
+def generate_unique_id():
+    unique_id1 = str(uuid.uuid4()). replace('-', '')
+    unique_id = unique_id1[:20]
+    return unique_id
+
+conn = sqlite3.connect('DBCantina.db')
+c = conn.cursor()
+
+c.execute(' ')
