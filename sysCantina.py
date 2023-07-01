d81a28c040b396039a41f9258f9ade41cd5c082a
maximofrias522 <129902079+maximofrias522@users.noreply.github.com>
2023-07-01 15:44:02 -0300

    s

--- sysCantina.py
+++ sysCantina.py
@@ -1,13 +1,30 @@
-import sqlite3
-import uuid 
-import kivi
+import customtkinter
 
-def generate_unique_id():
-    unique_id1 = str(uuid.uuid4()). replace('-', '')
-    unique_id = unique_id1[:20]
-    return unique_id
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
-conn = sqlite3.connect('DBCantina.db')
-c = conn.cursor()
+root = customtkinter.CTk()
+root.geometry("500x300")
 
-c.execute(' ')
+def login():
+    print("test")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System", text_font=("Roboto", 24))
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="x")
+entry1.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
